991970255798d0241ec3bb66eccea73d7f54dd4f
🍬 𝙹𝙾𝙻𝙻𝚈 𝙽𝚂 <159073116+1056516@users.noreply.github.com>
2025-12-16 19:45:09 -0500

    Add files via upload

---
+++ active/uv/uv.config.js
@@ -0,0 +1,7 @@
+self.__uv$config = {
+    prefix: '/active/go/',
+    bare:'https://mkykg-github-io.vercel.app/ub/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/active/uv/uv.handler.js',
+}
